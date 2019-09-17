4636fb29729d0cc1c10f797bdb79c43a945810db
isaiaharroyo <54848930+isaiaharroyo@users.noreply.github.com>
2019-09-17 10:59:43 -0400

    Update java.js

--- java.js
+++ java.js
@@ -1,5 +1,19 @@
-var button = function()
+var work = function()
 {
-  var node = document.createElement("h1");
+  var node = document.createElement("h2");
+  console.log("node",node);
   node.innerText="k bet";
+  node.setAttribute("onclick","make()");
+    
+  var body = document.querySelector("body");
+    body.appendChild(node);
+    
+  var button = document.querySelector("#firstClick");
+
+  button.setAttribute("onclick",null);
+}
+
+var make = function()
+{
+    
 }
