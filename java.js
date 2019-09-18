d6d1dea31d8d7ee1c2e1ae703e87df1715f2cffa
isaiaharroyo <54848930+isaiaharroyo@users.noreply.github.com>
2019-09-18 19:36:24 -0400

    Update java.js

--- java.js
+++ java.js
@@ -1,8 +1,8 @@
-var work = function()
+var work=function()
 {
   var node = document.createElement("h2");
   console.log("node",node);
-  node.innerText="k bet";
+  node.innerText="There is a Centre student named Procras Tinator. Today they woke up and decided to be productive! To start the day, you:";
   node.setAttribute("onclick","make()");
     
   var body = document.querySelector("body");
@@ -13,7 +13,26 @@ var work = function()
   button.setAttribute("onclick",null);
 }
 
-var make = function()
+var work2=function()
+{
+    var node2 = document.createElement("h2");
+    console.log("node",node2);
+    node2.innerText="You chose to get breakfast early and start reviewing for your first class. Way to be productive!";
+    
+    var body = document.querySelector("body");
+    body.appendChild(node2);
+    
+    var button = document.querySelector("#secondClick");
+
+    button.setAttribute("onclick",null);
+}
+
+var make1 = function()
+{
+    
+}
+
+var make2 = function2()
 {
     
 }
